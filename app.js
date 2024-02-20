0f47ba709448d037fde233c210230703479938d8
Syed Abdul Rehman <imabdul555@gmail.com>
2024-02-20 23:54:39 +0500

    first commit before push

---
+++ app.js
@@ -0,0 +1,156 @@
+/* **************************************** */
+// CHAPTER 1 SOLUTIONS
+/* **************************************** */
+// Q1 SOL :-
+
+alert("Greetings... How r u");
+
+//Q2 Sol :-
+//alert("Please insert a valid password");
+
+//Q3 Sol :-
+//alert("Welcome to JS Land \n Happy Coding....");
+
+//Q4 Sol :-
+//alert("Welcome to JS Land ");
+//alert(  "Happy coding ! \n Prevent this page from creating additional dialogues ");
+
+/* **************************************** */
+// CHAPTER 2 SOLUTIONS
+/* **************************************** */
+
+//Q1 Sol :-
+
+//var userName;
+
+//Q2 sol :-
+//var myName = "Syed Abdul Rehman";
+
+//Q3 Sol :-
+
+//var message;
+//message = "Hello world ";
+//alert("Hello World");
+
+//Q4 Sol :-
+
+//var studentName, Age, Course;
+
+//studentName = "John Doe";
+//Age = 15;
+//Course = "Certified Mobile Application Development";
+
+//Q5 Sol :-
+
+//var pizza = "PIZZA \n PIZZ \n PIZ \n  PI \n P";
+//alert(pizza);
+
+//Q6 Sol :-
+
+//var email = "syedarahman010@gmail.com";
+//alert("My email address is " + email);
+
+//Q7 Sol :-
+//var book = "A smarter way to learn Javascipt";
+//alert("I am trying to learn from the book " + book);
+
+//Q8 Sol :-
+//document.write("Yeah i can write content through Javascript");
+
+//Q9 Sol :-
+//var a = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+//alert(a);
+
+/* **************************************** */
+// CHAPTER 3 SOLUTIONS
+/* **************************************** */
+
+//Q1 Sol :-
+//var age = 26;
+//alert("I am " + age + " Years Old");
+
+//Q2 Sol :-
+//var visited = 14;
+//alert("You have visited the site " + visited + " times");
+
+//Q3 Sol :-
+//var birthYear = 1998;
+//document.write(  "My BirthYear is " +    birthYear +    " \n Data type of my declared variable is " +    typeof birthYear);
+
+//Q4 Sol :-
+//var VisitorName = "John Doe";
+//var ProductTitle = "Tshirt(s)";
+//var Order = 5;
+//document.write(VisitorName +"ordered " + Order +" " + ProductTitle +" on Xyz clothing store");
+
+/* **************************************** */
+// CHAPTER 1 -3  SOLUTIONS
+/* **************************************** */
+//------------------------------
+//         CHAPTER 1 -- ALERTS
+//------------------------------
+
+//Q1 SOL :-
+// var firstName = "user 1";
+// var lastName = "user 2";
+// var email = "test@gmail.com";
+// var phone = "0380983000";
+// var password = "ikjf88";
+
+// alert(firstName);
+// alert(lastName);
+// alert(email);
+// alert(phone);
+// alert(password);
+
+//Q2 Sol :-
+// alert("You are learning Javascript !");
+
+//Q3 Sol :-
+// alert("How r u ................. !");
+
+//--------------------------------------------
+//         CHAPTER 2 -- VARIABLES FOR STRINGS
+//--------------------------------------------
+
+// Q1 Sol :-
+// var camelCase;
+
+// Q2 Sol :-
+// var myVariable;
+// myVariable = "hello world again ..................";
+
+//Q3 Sol :-
+// var teamName;
+// teamName = "Team 1";
+// alert(teamName);
+
+//Q4 Sol :-
+// var bestMan = "Charlie";
+// bestMan = "User 1";
+
+//--------------------------------------------
+//         CHAPTER 3 -- VARIABLES FOR Numbers
+//--------------------------------------------
+// Q1 Sol :-
+// var caseQty;
+
+// Q2 Sol :-
+// caseQty = 144;
+
+// Q3 Sol :-
+// var num = 9;
+
+// Q4 Sol :-
+// var numSum ;
+// numSum = 2+10;
+
+// Q5 Sol :-
+// var merchTotal = 100;
+// var shippingCharge = 10;
+// var orderTotal = merchTotal + shippingCharge;
+// console.log(orderTotal);
+
+// Q6 Sol :-
+// var numTotal = 8;
+// numTotal = numTotal + 2;
